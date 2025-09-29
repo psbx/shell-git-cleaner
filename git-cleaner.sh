9c9336e1597f39f4c44b97ccf72b861a0cc8f60a
Serdar <36057288+psbx@users.noreply.github.com>
2025-09-29 08:06:05 +0300

    Replace egrep with portable grep -E

--- git-cleaner.sh
+++ git-cleaner.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 git fetch --all --prune
-CURRENT=$(git branch --show-current)
-for b in $(git branch --merged | egrep -v "(^\*|main|master|dev)"); do
-  echo "Deleting $b"
-  git branch -d "$b"
+current=$(git branch --show-current)
+
+git branch --merged | sed "s/^..//" | while read -r branch; do
+  case "$branch" in
+    "$current"|"main"|"master"|"dev")
+      continue
+      ;;
+  esac
+  echo "Deleting $branch"
+  git branch -d "$branch"
 done
-echo "Current branch: $CURRENT"
+
+echo "Current branch: $current"
